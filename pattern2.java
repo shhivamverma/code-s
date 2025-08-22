37aab99edcde5b5312e46dc8495b712ad2d8f878
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-08-22 23:56:29 +0530

    Update pattern2.java
    pattern 2
    
    
    Signed-off-by: SHIVAM VERMA <shivamvermaknp9922@gmail.com>

--- pattern2.java
+++ pattern2.java
@@ -1,9 +1,9 @@
 class pattern2
 {
-    public static void main(String args[])
-    {
-        for(int i=1;i<=4;i++)
-        {
+    public static void main(String args[])                                            //   *****
+    {                                                                                 //   *   *
+        for(int i=1;i<=4;i++)                                                         //   *   * 
+        {                                                                             //   *****
             for(int j=1;j<=5;j++)
             {
                 if(i==1 || j==1 || i==4 || j==5)
@@ -18,4 +18,5 @@ class pattern2
             System.out.println();
         }
     }
+
 }
